32f49f9261c752e0288959e05cdcc7d9a384ba55
elishandreaj <jimeneaelisha@gmail.com>
2026-02-27 13:17:12 +0800

    Fix toast component

--- frontend-ecasvote/app/layout.tsx
+++ frontend-ecasvote/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "../components/ui/toaster";
+import { Toaster } from "../components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,18 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        <Toaster
+          theme="light"
+          position="bottom-right"
+          duration={2500}
+          closeButton
+          toastOptions={{
+            classNames: {
+              title: "text-sm font-semibold",
+              description: "text-sm text-muted-foreground",
+            },
+          }}
+        />
       </body>
     </html>
   );
